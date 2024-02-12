6a0c2c7a56d135dd0134f7fe515f65d67310e7cd
Pradap Velladurai <92291187+Pradap08@users.noreply.github.com>
2024-02-12 14:40:32 +0530

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -4,7 +4,7 @@ def call(String hostip){
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
-        def curll  = "curl -X PUT -u '${USER}:${PASS}' -T target/*.jar http://${hostip}:8082/artifactory/example-repo-local/"
+        def curll  = "curl -X PUT -u '${USER}:${PASS}' -T target/*.jar http://${hostip}:8081/artifactory/example-repo-local/"
         sh curll
     }
 }
